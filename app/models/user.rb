ae2ba634758860904ba5e656dba28db20d35c34c
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-09-25 19:57:46 +0900

    単体テストコード記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,5 +18,5 @@ class User < ApplicationRecord
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-  
+
 end

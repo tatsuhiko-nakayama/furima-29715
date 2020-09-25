cfb6209cd959636dfc2bd6ade0e07e8434636642
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-09-25 20:06:50 +0900

    Rubocop修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname {Faker::Name.initials(number: 2)}
-    email {Faker::Internet.free_email}
-    password {"test1234"}
-    password_confirmation {password}
-    last_name {"中山"}
-    first_name {"達彦"}
-    last_name_reading {"ナカヤマ"}
-    first_name_reading {"タツヒコ"}
-    birthday {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
+    nickname { Faker::Name.initials(number: 2) }
+    email { Faker::Internet.free_email }
+    password { 'test1234' }
+    password_confirmation { password }
+    last_name { '中山' }
+    first_name { '達彦' }
+    last_name_reading { 'ナカヤマ' }
+    first_name_reading { 'タツヒコ' }
+    birthday { Faker::Date.between(from: '1930-01-01', to: '2015-12-31') }
   end
 end

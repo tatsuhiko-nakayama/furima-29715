5c7f1becf88c4c1e04eaeb2ec09eefffc64cac38
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-09-26 19:41:18 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,10 +9,11 @@ class Item < ApplicationRecord
   belongs_to_active_hash :ship_from
   belongs_to_active_hash :ship_day
 
-  validates :name, :text, :price, presence: true
+  validates :name, :text, :price, :image, presence: true
   validates :name, length: { maximum: 40 }
   validates :text, length: { maximum: 1000 }
   validates :price, format: { with: /\A[0-9]+\z/, message: 'Half-width number' }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
-  validates :genre, :condition, :ship_fee, :ship_from, :ship_day, :numericality { other_than: 1, message: 'Select' } 
+  validates :genre_id, :condition_id, :ship_fee_id, :ship_from_id, :ship_day_id, numericality: { other_than: 1, message: 'Select' }
+
 end

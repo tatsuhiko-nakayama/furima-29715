e1afaf857b528ab393e06a54c9ca3525b03717fb
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-09-27 18:23:05 +0900

    編集・更新機能の実装
    ・編集ページへの遷移を出品者に限定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :move_to_show, only: :edit
+
   def index
     @items = Item.all.order("created_at DESC")
   end
@@ -17,6 +19,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
   end
 
   def show
@@ -27,5 +39,12 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :text, :price, :genre_id, :condition_id, :ship_fee_id, :ship_from_id, :ship_day_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_show
+    @item = Item.find(params[:id])
+    unless current_user == @item.user.id
+      redirect_to item_path(@item.id)
+    end
   end
 end

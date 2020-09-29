c381485244114ab31b08b657961f6837fb9da7c3
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-09-29 17:55:14 +0900

    Formオブジェクトの冗長記述を修正

--- app/forms/order_address.rb
+++ app/forms/order_address.rb
@@ -14,9 +14,7 @@ class OrderAddress
   validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
 
   def save
-    user = User.find(user_id)
-    item = Item.find(item_id)
-    order = Order.create(user_id: user.id, item_id: item.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
 end

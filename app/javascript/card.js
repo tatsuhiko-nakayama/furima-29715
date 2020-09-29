0a7de52c8442ffc44226136db5400808014f6d26
tatsuhiko-nakayama <gongoniiyo114@gmail.com>
2020-09-29 16:42:53 +0900

    パラメータにカード情報を含ませない処理

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -21,7 +21,10 @@ const pay = () => {
         const tokenObj = `<input value=${token} type="hidden" name='token'>`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-     
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
       document.getElementById("charge-form").reset();
